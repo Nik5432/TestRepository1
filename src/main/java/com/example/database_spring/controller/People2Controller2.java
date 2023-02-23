54b978172e729fc1ef452acb302bad74cdb692af
Nikita <nik5432@lenta.ru>
2023-02-23 21:02:44 +0300

    commit new model

---
+++ src/main/java/com/example/database_spring/controller/People2Controller2.java
@@ -0,0 +1,25 @@
+package com.example.database_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class People2Controller2 {
+//
+//    @Autowired
+//    private PeopleService peopleService;
+//
+//    @GetMapping("/test11")
+//    public HttpStatus mainPage12() { /*@RequestParam String name, @RequestParam String surname*/
+//        //peopleService.saveByParams(surname, name);
+//        return HttpStatus.OK;
+//    }
+
+    @GetMapping("/my-1-main")
+    public void mainPage12() { /*@RequestParam String name, @RequestParam String surname*/
+        //peopleService.saveByParams(surname, name);
+        //return HttpStatus.OK;
+        String s = "";
+    }
+}
+
---
+++ src/main/java/com/example/database_spring/model/People2.java
@@ -0,0 +1,24 @@
+package com.example.database_spring.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Table
+@Entity
+public class People2 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String surname;
+
+    public People2(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    protected People2() {
+    }
+}
