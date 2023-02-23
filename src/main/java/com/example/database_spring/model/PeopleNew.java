83631b95649bd8765afbab72a9b8f5264bf7a911
Nikita <nik5432@lenta.ru>
2023-02-23 21:08:10 +0300

    update id in new model

---
+++ src/main/java/com/example/database_spring/model/PeopleNew.java
@@ -0,0 +1,24 @@
+package com.example.database_spring.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Table
+@Entity
+public class PeopleNew {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id_update;
+    private String name;
+    private String surname;
+
+    public PeopleNew(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    protected PeopleNew() {
+    }
+}
